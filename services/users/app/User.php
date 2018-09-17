c1d5f0b934e5abb5a73edec8a93ccd64c20ec476
vinifig <vinifig@hotmail.com>
2018-09-17 11:58:04 -0300

    rename token field

--- services/users/app/User.php
+++ services/users/app/User.php
@@ -26,4 +26,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function token() {
+        $this->token = str_random(60);
+        $this->save();
+    }
 }
